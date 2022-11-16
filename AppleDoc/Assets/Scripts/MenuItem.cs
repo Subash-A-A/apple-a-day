83bb65866deddea78dfb83647bb9a844c5e9879c
Subash <it.subashaa@gmail.com>
2022-11-16 11:51:47 +0530

    lighting Init

--- AppleDoc/Assets/Scripts/MenuItem.cs
+++ AppleDoc/Assets/Scripts/MenuItem.cs
@@ -7,20 +7,33 @@ public class MenuItem : MonoBehaviour
     [SerializeField] Color hoverColor;
     [SerializeField] Image pieImg;
 
+    private Vector3 localScale;
+
     private void Start()
     {
         pieImg.color = baseColor;
+        localScale = Vector3.one;
+    }
+
+    private void Update()
+    {
+        ScaleLerper();
     }
 
     public void Select()
     {
         pieImg.color = hoverColor;
-        transform.localScale = new Vector3(1.1f, 1.1f, 1.1f);
+        localScale = new Vector3(1.1f, 1.1f, 1.1f);
     }
 
     public void DeSelect()
     {
         pieImg.color = baseColor;
-        transform.localScale = Vector3.one;
+        localScale = Vector3.one;
+    }
+
+    private void ScaleLerper()
+    {
+        transform.localScale = Vector3.Lerp(transform.localScale, localScale, 10 * Time.unscaledDeltaTime);
     }
 }
